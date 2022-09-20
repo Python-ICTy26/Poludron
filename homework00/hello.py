687556f85fff4ecbfa8c7c5098d2cad2e8ea7cf9
Poludron <polukhinalex2004@gmail.com>
2022-09-20 21:16:36 +0300

    Добавлено решение для функции приветствия

--- homework00/hello.py
+++ homework00/hello.py
@@ -1,6 +1,8 @@
 def get_greeting(name: str) -> str:
-    pass
-
+    if name == "World":
+        return "Hello, World!"
+    if name == "Anonymous":
+        return "Hello, Anonymous!"
 
 if __name__ == "__main__":
     message = get_greeting("World")
