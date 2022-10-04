e353e2eed30095dc218334fdde87b2f35a6d0159
Poludron <polukhinalex2004@gmail.com>
2022-10-04 17:26:40 +0300

    Реализована функция encrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -2,45 +2,47 @@ import typing as tp
 
 
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
-    """
-    Encrypts plaintext using a Caesar cipher.
-
-    >>> encrypt_caesar("PYTHON")
-    'SBWKRQ'
-    >>> encrypt_caesar("python")
-    'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
-    'Sbwkrq3.6'
-    >>> encrypt_caesar("")
-    ''
-    """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for i in plaintext:
+        if i.isalpha():
+            if i.isupper():
+                ciphertext += chr(((ord(i) - ord("A")) + shift) % 26 + ord("A"))
+            else:
+                ciphertext += chr(((ord(i) - ord("a")) + shift) % 26 + ord("a"))
+        else:
+            ciphertext += i
     return ciphertext
 
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
-    """
-    Decrypts a ciphertext using a Caesar cipher.
-
-    >>> decrypt_caesar("SBWKRQ")
-    'PYTHON'
-    >>> decrypt_caesar("sbwkrq")
-    'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
-    >>> decrypt_caesar("")
-    ''
-    """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for i in ciphertext:
+        if i.isalpha():
+            if i.isupper():
+                plaintext += chr(((ord(i) - ord("A")) - shift) % 26 + ord("A"))
+            else:
+                plaintext += chr(((ord(i) - ord("a")) - shift) % 26 + ord("a"))
+        else:
+            plaintext += i
     return plaintext
 
+print(decrypt_caesar("SBWKRQ"))
 
-def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
+'''def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
     """
     Brute force breaking a Caesar cipher.
     """
     best_shift = 0
     # PUT YOUR CODE HERE
-    return best_shift
+    return best_shift'''
+
+
+'''for i in plaintext:
+        if i.isalpha():
+            stayInAlphabet = ord(i) + shift 
+            if stayInAlphabet > ord('z'):
+                stayInAlphabet -= 26
+            finalLetter = chr(stayInAlphabet)
+            ciphertext += finalLetter
+        else:
+            plaintext += i'''
