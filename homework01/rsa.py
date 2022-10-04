507aa054ecc77d0c9962e0fa45cb11c2e9dc6c10
Poludron <polukhinalex2004@gmail.com>
2022-10-04 20:46:55 +0300

    Реализована функция is_prime(n)

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -13,8 +13,7 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    # PUT YOUR CODE HERE
-    pass
+    return n>1 and all(n % i != 0 for i in range(2, int(n**0.5)+1))
 
 
 def gcd(a: int, b: int) -> int:
