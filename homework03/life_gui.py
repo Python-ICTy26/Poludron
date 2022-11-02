489d77f2c6fa6e668cd6cdba649a2e78b7cdaddc
Poludron <polukhinalex2004@gmail.com>
2022-11-02 20:47:46 +0300

    Реализована функция draw_lines(), draw_grid() и класс GUI

--- homework03/life_gui.py
+++ homework03/life_gui.py
@@ -7,14 +7,33 @@ from ui import UI
 class GUI(UI):
     def __init__(self, life: GameOfLife, cell_size: int = 10, speed: int = 10) -> None:
         super().__init__(life)
+        self.speed = speed
+        self.width = life.rows * cell_size
+        self.height = life.cols * cell_size
+        self.cell_size = cell_size
+        self.screen = pygame.display.set_mode((self.width, self.height))
 
     def draw_lines(self) -> None:
         # Copy from previous assignment
-        pass
+        for x in range(0, self.width, self.cell_size):
+            pygame.draw.line(self.screen, pygame.Color("black"), (x, 0), (x, self.height))
+        for y in range(0, self.height, self.cell_size):
+            pygame.draw.line(self.screen, pygame.Color("black"), (0, y), (self.width, y))
 
     def draw_grid(self) -> None:
         # Copy from previous assignment
-        pass
+        for x in range(self.life.rows):
+            for y in range(self.life.cols):
+                pygame.draw.rect(
+                    self.screen,
+                    pygame.Color("green") if self.life.curr_generation[x][y] == 1 else pygame.Color("white"),
+                    (
+                        y * self.cell_size,
+                        x * self.cell_size,
+                        self.cell_size,
+                        self.cell_size,
+                    ),
+                )
 
     def run(self) -> None:
         # Copy from previous assignment
