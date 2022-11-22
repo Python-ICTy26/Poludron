7a86593135bde6e8698523173f61d4ca21c5ab00
Poludron <polukhinalex2004@gmail.com>
2022-11-22 23:29:02 +0300

    Добавлено решение функции repo_find()

--- homework04/pyvcs/repo.py
+++ homework04/pyvcs/repo.py
@@ -4,8 +4,16 @@ import typing as tp
 
 
 def repo_find(workdir: tp.Union[str, pathlib.Path] = ".") -> pathlib.Path:
-    # PUT YOUR CODE HERE
-    ...
+    gitdir = os.getenv("GIT_DIR", ".pyvcs")
+    workdir = pathlib.Path(workdir)
+    while pathlib.Path("/") != workdir.absolute():
+        if (workdir / gitdir).is_dir():
+            return workdir / gitdir
+        workdir = workdir.parent
+    if (workdir / gitdir).is_dir():
+        return workdir / gitdir
+    else:
+        raise Exception("Not a git repository")
 
 
 def repo_create(workdir: tp.Union[str, pathlib.Path]) -> pathlib.Path:
