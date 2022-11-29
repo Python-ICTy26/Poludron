23300b95d07cd022aa356d2ab7f38902590eb656
Poludron <polukhinalex2004@gmail.com>
2022-11-29 21:37:03 +0300

    Добавлено решение функции get_mutual()

--- homework05/vkapi/friends.py
+++ homework05/vkapi/friends.py
@@ -77,4 +77,51 @@ def get_mutual(
     :param offset: Смещение, необходимое для выборки определенного подмножества общих друзей.
     :param progress: Callback для отображения прогресса.
     """
-    pass
+    start = Session(config.VK_CONFIG["domain"])
+    all_friends = []
+    if target_uids:
+        for i in range(((len(target_uids) - 1) // 100) + 1):
+            try:
+                mutual_friends = start.get(
+                    "friends.getMutual",
+                    params={
+                        "access_token": config.VK_CONFIG["access_token"],
+                        "v": config.VK_CONFIG["version"],
+                        "source_uid": source_uid,
+                        "target_uid": target_uid,
+                        "target_uids": ",".join(list(map(str, target_uids))),
+                        "order": order,
+                        "count": 100,
+                        "offset": i * 100,
+                    },
+                )
+                for friend in mutual_friends.json()["response"]:
+                    all_friends.append(
+                        MutualFriends(
+                            id=friend["id"],
+                            common_friends=list(map(int, friend["common_friends"])),
+                            common_count=friend["common_count"],
+                        )
+                    )
+            except:
+                pass
+            time.sleep(0.5)
+        return all_friends
+    try:
+        mutual_friends = start.get(
+            "friends.getMutual",
+            params={
+                "access_token": config.VK_CONFIG["access_token"],
+                "v": config.VK_CONFIG["version"],
+                "source_uid": source_uid,
+                "target_uid": target_uid,
+                "target_uids": target_uids,
+                "order": order,
+                "count": count,
+                "offset": offset,
+            },
+        )
+        all_friends.extend(mutual_friends.json()["response"])
+    except:
+        pass
+    return all_friends
