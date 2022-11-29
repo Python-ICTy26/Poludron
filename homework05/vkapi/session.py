6da36000c6f3d0ebc4aea6901af7b045fa4d45c8
Poludron <polukhinalex2004@gmail.com>
2022-11-29 21:27:08 +0300

    Добавлено решение post-запроса

--- homework05/vkapi/session.py
+++ homework05/vkapi/session.py
@@ -27,7 +27,9 @@ class Session:
     def get(self, url: str, *args: tp.Any, **kwargs: tp.Any) -> requests.Response:
         kwargs["timeout"] = kwargs["timeout"] if "timeout" in kwargs else self.timeout
         response = self.session.get(self.base_url + "/" + url, *args, **kwargs)
-        return
+        return response
 
 def post(self, url: str, *args: tp.Any, **kwargs: tp.Any) -> requests.Response:
-        pass
+    kwargs["timeout"] = kwargs["timeout"] if "timeout" in kwargs else self.timeout
+    response = self.session.post(self.base_url + "/" + url, *args, **kwargs)
+    return response
