a5bb9ab6c52bccb1daa3552b030ce5a28c65c483
Poludron <87179249+Poludron@users.noreply.github.com>
2022-12-14 13:00:02 +0300

    Done extract_news() and extract_next_page

--- homework06/scraputils.py
+++ homework06/scraputils.py
@@ -3,11 +3,30 @@ from bs4 import BeautifulSoup
 
 
 def extract_news(parser):
-    """ Extract news from a given web page """
+    """Extract news from a given web page"""
+    body = parser.findAll("table")[2]
+    titles = [title.text for title in body.findAll("a", {"class": "titlelink"})]
+    authors = [user.text for user in body.findAll("a", {"class": "hnuser"})]
+    urls = [link["href"] for link in body.findAll("a", {"class": "titlelink"})]
+    urls = ["https://news.ycombinator.com/" + url if url[:4] == "item" else url for url in urls]
+    points = [score.text.split()[0] for score in body.findAll("span", {"class": "score"})]
+    ids = [post["id"] for post in body.findAll("tr", {"class": "athing"})]
+    discussions = [
+        body.findAll("span", {"id": f"unv_{id}"})[0].findNext("a", {"href": f"item?id={id}"}).text
+        for id in ids
+    ]
+    comments = [0 if element.isalpha() else int(element.split()[0]) for element in discussions]
     news_list = []
-
-    # PUT YOUR CODE HERE
-
+    for i, _ in enumerate(titles):
+        news_list.append(
+            {
+                "author": authors[i],
+                "comments": comments[i],
+                "points": points[i],
+                "title": titles[i],
+                "url": urls[i],
+            }
+        )
     return news_list
 
 
